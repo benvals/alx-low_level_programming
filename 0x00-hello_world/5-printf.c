0d58345b457a5e22d3e35a31ecb68635a812506f
benvals <aegbeigwe@gmail.com>
2023-06-15 15:02:10 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,7 +4,6 @@
  * Return: 0 (success)
  */
 int main(void)
-{
-	printf("with proper grammar, but the outcome is a piece of art,\n")
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
